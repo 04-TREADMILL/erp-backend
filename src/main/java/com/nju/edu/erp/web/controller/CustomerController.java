2c8e580279cdfc489885dd296818e4c4a20bc493
201250007 <201250007@smail.nju.edu.cn>
2022-06-28 10:37:59 +0800

    增加用户管理权限设置

--- src/main/java/com/nju/edu/erp/web/controller/CustomerController.java
+++ src/main/java/com/nju/edu/erp/web/controller/CustomerController.java
@@ -1,6 +1,8 @@
 package com.nju.edu.erp.web.controller;
 
+import com.nju.edu.erp.auth.Authorized;
 import com.nju.edu.erp.enums.CustomerType;
+import com.nju.edu.erp.enums.Role;
 import com.nju.edu.erp.model.vo.CustomerVO;
 import com.nju.edu.erp.service.CustomerService;
 import com.nju.edu.erp.web.Response;
@@ -19,23 +21,27 @@ public class CustomerController {
     }
 
     @GetMapping("/findByType")
+    @Authorized(roles = {Role.ADMIN, Role.GM, Role.SALE_MANAGER, Role.SALE_STAFF})
     public Response findByType(@RequestParam CustomerType type) {
         return Response.buildSuccess(customerService.getCustomersByType(type));
     }
 
     @PostMapping("/add")
+    @Authorized(roles = {Role.ADMIN, Role.GM, Role.SALE_MANAGER, Role.SALE_STAFF})
     public Response addCustomer(@RequestBody CustomerVO customerVO) {
         customerService.addCustomer(customerVO);
         return Response.buildSuccess();
     }
 
     @GetMapping("/delete")
+    @Authorized(roles = {Role.ADMIN, Role.GM, Role.SALE_MANAGER, Role.SALE_STAFF})
     public Response deleteCustomer(@RequestParam(value = "id") int id) {
         customerService.deleteCustomerById(id);
         return Response.buildSuccess();
     }
 
     @PostMapping("/update")
+    @Authorized(roles = {Role.ADMIN, Role.GM, Role.SALE_MANAGER, Role.SALE_STAFF})
     public Response updateCustomer(@RequestBody CustomerVO customerVO) {
         customerService.updateCustomer(customerVO);
         return Response.buildSuccess();
