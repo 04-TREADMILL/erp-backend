ec655ab6c85529ccb18ef70cbf3b2f637485f339
V_Galaxy <1904821183@qq.com>
2022-06-29 21:03:14 +0800

    fix: unwind CustomerPO in CustomerPurchaseAmountPO

--- src/main/java/com/nju/edu/erp/model/po/CustomerPurchaseAmountPO.java
+++ src/main/java/com/nju/edu/erp/model/po/CustomerPurchaseAmountPO.java
@@ -12,12 +12,54 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 @Builder
 public class CustomerPurchaseAmountPO {
-
     /**
-     * 客户
+     * 编号
      */
-    private CustomerPO customerPO;
-
+    private Integer id;
+    /**
+     * 分类(供应商\销售商)
+     */
+    private String type;
+    /**
+     * 级别（五级，一级普通用户，五级VIP客户）
+     */
+    private Integer level;
+    /**
+     * 姓名
+     */
+    private String name;
+    /**
+     * 电话号码
+     */
+    private String phone;
+    /**
+     * 地址
+     */
+    private String address;
+    /**
+     * 邮编
+     */
+    private String zipcode;
+    /**
+     * 电子邮箱
+     */
+    private String email;
+    /**
+     * 应收额度本公司给客户的信用额度，客户欠本公司的钱的总额不能超过应收额度）
+     */
+    private BigDecimal lineOfCredit;
+    /**
+     * 应收（客户还应付给本公司但还未付的钱）
+     */
+    private BigDecimal receivable;
+    /**
+     * 应付（本公司欠客户的钱）
+     */
+    private BigDecimal payable;
+    /**
+     * 默认业务员
+     */
+    private String operator;
     /**
      * 用户在某时间段内通过某销售人员购买的商品的总金额(不考虑退货情况,销售单不需要审批通过)
      */
