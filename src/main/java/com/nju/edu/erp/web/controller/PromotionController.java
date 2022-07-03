db38ebf13a9b0cd1e11fac65d0202b8e39b14b85
201250007 <201250007@smail.nju.edu.cn>
2022-07-03 15:04:33 +0800

    促销策略管理与前端的接口

---
+++ src/main/java/com/nju/edu/erp/web/controller/PromotionController.java
@@ -0,0 +1,61 @@
+package com.nju.edu.erp.web.controller;
+
+import com.nju.edu.erp.auth.Authorized;
+import com.nju.edu.erp.enums.Role;
+import com.nju.edu.erp.exception.MyServiceException;
+import com.nju.edu.erp.service.strategy.PromotionStrategy;
+import com.nju.edu.erp.web.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "/promotion")
+public class PromotionController {
+
+    private final PromotionStrategy promotionStrategy;
+
+    @Autowired
+    public PromotionController(PromotionStrategy promotionStrategy) {
+        this.promotionStrategy = promotionStrategy;
+    }
+
+    @PostMapping("/add")
+    @Authorized(roles = {Role.GM, Role.ADMIN})
+    public Response addPromotion(@RequestParam("promotionType") String promotionType,
+                                 @RequestBody Object promotionVO) {
+        try {
+            promotionStrategy.addPromotion(promotionType, promotionVO);
+            return Response.buildSuccess();
+        } catch (MyServiceException e) {
+            return Response.buildFailed(e.getCode(), e.getMessage());
+        } catch (Exception e) {
+            return Response.buildFailed("111111", "Unknown Exception");
+        }
+    }
+
+    @GetMapping("/delete")
+    @Authorized(roles = {Role.GM, Role.ADMIN})
+    public Response deletePromotion(@RequestParam("promotionType") String promotionType,
+                                    @RequestParam("promotionId") int promotionId) {
+        try {
+            promotionStrategy.deletePromotionById(promotionType, promotionId);
+            return Response.buildSuccess();
+        } catch (MyServiceException e) {
+            return Response.buildFailed(e.getCode(), e.getMessage());
+        } catch (Exception e) {
+            return Response.buildFailed("111111", "Unknown Exception");
+        }
+    }
+
+    @GetMapping("/show")
+    @Authorized(roles = {Role.GM, Role.ADMIN})
+    public Response showPromotion(@RequestParam("promotionType") String promotionType) {
+        try {
+            return Response.buildSuccess(promotionStrategy.getPromotions(promotionType));
+        } catch (MyServiceException e) {
+            return Response.buildFailed(e.getCode(), e.getMessage());
+        } catch (Exception e) {
+            return Response.buildFailed("111111", "Unknown Exception");
+        }
+    }
+}
