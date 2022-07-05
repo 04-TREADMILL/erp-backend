dc25bd35b5538f16dba99d971e48864e997b6a8f
V_Galaxy <1904821183@qq.com>
2022-07-05 22:18:34 +0800

    feat: calculate original salary by punch time

--- src/main/java/com/nju/edu/erp/dao/SalarySheetDao.java
+++ src/main/java/com/nju/edu/erp/dao/SalarySheetDao.java
@@ -11,6 +11,8 @@ import java.util.List;
 @Mapper
 public interface SalarySheetDao {
 
+    SalarySheetPO getLatestByEmployeeId(int employeeId);
+
     SalarySheetPO getLatest();
 
     int save(SalarySheetPO toSave);
